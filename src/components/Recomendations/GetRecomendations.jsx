41de3cc4d1070df56568ebb4f9c4a5e3563e433a
Evan Freymiller <evanfreymiller@Evans-MBP.lan>
2019-03-20 23:30:30 -0500

    added get rec change

--- src/components/Recomendations/GetRecomendations.jsx
+++ src/components/Recomendations/GetRecomendations.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Button } from 'antd';
 import Metric from '../Body/Metric';
+import { Steps, Icon } from 'antd';
+import { LIGHT_GREY } from './../../Constants';
+const Step = Steps.Step;
+
 
 const GetRecomendations = (props) => {
     return (
         <div>
             <div className="width-100">
                 <Metric
-                    title="Recommendations"
-                    label="Get Buy, Hold, and Sell Ratings"
+                    title="Should You Buy"
+                    label="Buy, Hold, or Sell"
                     center
                     width={'100%'}
-                    titleFontSize={18}
+                    titleFontSize={14}
                     fontWeight={900}
                     duration={1}
                     decimals={0}
@@ -19,7 +23,13 @@ const GetRecomendations = (props) => {
                     prefix={'$'} />
             </div>
             <div className="shares-divider"> </div>
-            <Button onClick={props.showRecomendations} className="width100 loaf-button">Get Recomendations</Button>
+            <Button onClick={props.showRecomendations} className="width100 loaf-button">Get</Button>
+            <div style={{marginTop: 10, paddingTop: 10}}> </div>
+            <Steps>
+                <Step icon={<Icon style={{ color: LIGHT_GREY }} type="login" />} />
+                <Step icon={<Icon style={{ color: LIGHT_GREY }} type="sync" />} />
+                <Step icon={<Icon style={{ color: LIGHT_GREY }} type="logout" />} />
+            </Steps>
         </div>
     )
 }
